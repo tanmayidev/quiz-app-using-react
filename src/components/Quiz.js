58d984b4400038fe9fcef676f5812042936616e1
tanmayidev <tanmayi.d.99@gmail.com>
2023-05-11 16:27:09 +0530

    create global state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,33 +1,16 @@
-import { useReducer, useContext } from "react";
+import { useContext } from "react";
 import Question from "./Question";
 import { QuizContext } from "../contexts/quiz";
 
-const initialState = {
-  currentQuestionIndex: 0,
-  questions: [],
-};
-
-const reducer = (state, action) => {
-  if (action.type === "NEXT_QUESTION") {
-    return {
-      ...state,
-      currentQuestionIndex: state.currentQuestionIndex + 1,
-    };
-  }
-  return state;
-};
-
 const Quiz = () => {
   // quiz context - allows you to access values in QuizContext
-  const quizState = useContext(QuizContext);
-  console.log("quizState", quizState);
-  // not using useState to maintain business logic outside the component and to reduce the number of useStates used
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="quiz">
       <div>
         <div className="score">Question 1/8</div>
-        <Question questions={state.questions} />
+        <Question />
         <div
           className="next-button"
           onClick={() => dispatch({ type: "NEXT_QUESTION" })}
--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,10 +1,28 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+
+const initialState = {
+  currentQuestionIndex: 0,
+  questions: [],
+};
+
+const reducer = (state, action) => {
+  if (action.type === "NEXT_QUESTION") {
+    return {
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    };
+  }
+  return state;
+};
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
+  // not using useState to maintain business logic outside the component and to reduce the number of useStates used
+  const value = useReducer(reducer, initialState);
+
   // value attribute takes in value that will become accessible to all children of QuizContext.Provider
   // children all the components which are wrapped with QuizProvider Component
   // in our case the QuizProvider has Quiz component as child - see in index.js file
-  return <QuizContext.Provider value="fooooo">{children}</QuizContext.Provider>;
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
